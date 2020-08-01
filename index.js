c4b24b5a80827fff1bc47bde9844f2a0107839b3
Andres Rivera <soulkalir@gmail.com>
2020-08-01 17:03:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,10 +23,9 @@ function johnLennonFacts(array){
 function iLoveTheBeatles(number){
   var array = [];
   do{
-  array.push(`I love the Beatles!`)
+  array.push(`I love the Beatles!`);
   number++;
   }
-  while(number<15){
-  }
-  
+  while(number<15);
+  return array;
 }
