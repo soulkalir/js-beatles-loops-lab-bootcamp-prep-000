e9b0c2c4ab9f7923eb8039da26680b9be708a4b9
Andres Rivera <soulkalir@gmail.com>
2020-08-01 16:50:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,48 +1,20 @@
 // add solution here
 
-
-
-function theBeatlesPlay(musicians, instruments){
- 
-  var arrayPhrases = [];
-  
-  for(let i = 0; i<musicians.length; i++){
-   arrayPhrases.push(`${musicians[i]} plays ${instruments[i]}`)
-  } //end of loop
-  return arrayPhrases;
+function theBeatlesPlay(musicians,instruments){
+  var newArray=[];
+  for(let i=0; i<musicians.length; i++){
+    newArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return newArray;
 }
+
 
 
 function johnLennonFacts(array){
-  let i = 0;
-  while(i <= array.length - 1){
-    array[i] = array[i] + '!!!';
-    i += 1; 
-    
+  let i=0;
+  while(i<=array.length-1){
+   array.push(`${array[i]}+!!!`)
+    i++;
   }
   return array;
 }
-
-function iLoveTheBeatles(number){
-  var array=[];
-  do{
-  array.push('I love the Beatles!');
-  number++;
-  } while(number<15);
-
-return array;
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
